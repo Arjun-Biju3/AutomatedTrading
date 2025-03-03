d7b60d51e0619622f80a41387e001aadd8cdc2d5
Nandhukrishnanv2 <155341719+Nandhukrishnanv2@users.noreply.github.com>
2025-03-03 09:21:10 +0530

    design completed partially

--- trading/views.py
+++ trading/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+def login_view(request):
+    return render(request, 'login.html')
+
+def home(request):
+    return render(request, 'index.html')
+
+def register_view(request):
+    return render(request, 'register.html')
+
+def home_view(request):
+    return render(request, 'home.html')
